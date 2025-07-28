f3ce8f9b2423292576e7a24212102f98baf40efc
samyyc <3356207189@qq.com>
2025-07-28 16:27:11 +0800

    ci: fix

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 fn main() {
     prost_build::Config::new()
         .out_dir("src/pb")
-        .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(&["orwell.proto"], &["."])
         .unwrap();
 }
